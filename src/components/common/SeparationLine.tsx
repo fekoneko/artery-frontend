2629c6157693f7d2b7fdf479e9818d719ba1105c
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-04-08 18:36:05 +0300

    navigation styles

--- src/components/common/SeparationLine.tsx
+++ src/components/common/SeparationLine.tsx
@@ -1,8 +1,8 @@
-const LandingSeparationLine = () => {
+const SeparationLine = () => {
   return (
     <div className="w-full px-[12%]">
       <div className="h-[2px] w-full bg-emerald-400/80" />
     </div>
   );
 };
-export default LandingSeparationLine;
+export default SeparationLine;
--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,8 +1,56 @@
+import { Link, useLocation } from 'react-router-dom';
+import SeparationLine from '../common/SeparationLine';
+import { animated, useSpring } from 'react-spring';
+import { useMemo } from 'react';
+
+interface Tab {
+  title: string;
+  link: string;
+}
+const tabs: Tab[] = [
+  { title: 'Главная', link: '/feed' },
+  { title: 'Корзина', link: '/cart' },
+  { title: 'Профиль', link: '/profile' },
+];
+
 const Navigation = () => {
+  const location = useLocation();
+  const activeIndex = useMemo(
+    () => tabs.findIndex((tab) => location.pathname.includes(tab.link, 0)),
+    [location],
+  );
+  const selectorSpring = useSpring({
+    left: `calc((100% + 0.5rem) / ${tabs.length} * ${activeIndex})`,
+    width: `calc((100% + 0.5rem) / ${tabs.length} - 0.5rem)`,
+  });
+
   return (
-    <nav className="flex " role="navigation">
-      Navigation
-    </nav>
+    <>
+      <SeparationLine />
+      <nav role="navigation" className="px-[12%] py-2">
+        <div className="relative size-full">
+          <ul
+            style={{ gridTemplateColumns: `repeat(${tabs.length}, 1fr)` }}
+            className="grid size-full grid-flow-col gap-2"
+          >
+            {tabs.map((tab) => (
+              <li key={tab.title} className="flex">
+                <Link
+                  to={tab.link}
+                  className="flex h-10 grow items-center justify-center rounded-lg hover:bg-teal-100/[2.5%] hover:no-underline"
+                >
+                  {tab.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <animated.div
+            style={selectorSpring}
+            className="pointer-events-none absolute top-0 h-10 rounded-lg bg-teal-100/5"
+          />
+        </div>
+      </nav>
+    </>
   );
 };
 export default Navigation;
