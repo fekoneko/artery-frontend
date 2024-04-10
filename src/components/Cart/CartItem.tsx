c9bd88bb41c510abf293b78eaaac7cce3d42d170
MattKenyei <mattkenyei@gmail.com>
2024-04-10 21:45:53 +0300

    corrected CartItem

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { PropsWithChildren } from 'react';
+import { Good } from '../../@types/global';
 
-const CartItem = ({ children, onRemove }: PropsWithChildren<{ onRemove: (id: number) => void }>) => {
+const CartItem = ({ children, onRemove }: PropsWithChildren<{children:Good, onRemove: (id: number) => void }>) => {
   const { imageUrl, name, price, id } = children;
   const [quantity, setQuantity] = useState(1);
 
@@ -22,7 +23,7 @@ const CartItem = ({ children, onRemove }: PropsWithChildren<{ onRemove: (id: num
   
   return (
     <div className='flex flex-row justify-between items-center border-b border-gray-300 py-4'>
-      <div className='w-20 flex-shrink-0'>
+      <div className='w-20 shrink-0'>
         <img src={imageUrl} alt='' className='w-full' />
       </div>
       <div className='text-xl font-semibold max-w-24'>{name}</div>
@@ -32,7 +33,7 @@ const CartItem = ({ children, onRemove }: PropsWithChildren<{ onRemove: (id: num
         <button className='text-gray-500 hover:text-gray-900' onClick={increaseQuantity}>+</button>
       </div>
       <div className='text-xl font-semibold'>
-        Total: ${(price * quantity).toFixed(2)}
+        Total: {(price * quantity).toFixed(2)} RUB
       </div>
       <button className='text-red-500 hover:text-red-700' onClick={removeFromCart}>Remove</button>
     </div>
