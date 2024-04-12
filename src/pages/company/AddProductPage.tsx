2f0a8a9bec0c817918c1f6dbbbdb26b24ef23217
MattKenyei <mattkenyei@gmail.com>
2024-04-12 17:42:51 +0300

    added product adding for companies

--- src/pages/company/AddProductPage.tsx
+++ src/pages/company/AddProductPage.tsx
@@ -1,6 +1,10 @@
+import AddProductForm from "../../components/Forms/AddProductForm";
+
 const AddProductPage = () => {
   return (
-    <main className="flex size-full flex-col gap-4 overflow-y-scroll pl-[12%] pr-[calc(12%-0.5rem)]"></main>
+    <main className="flex mt-9 size-full flex-col gap-4 overflow-y-scroll pl-[12%] pr-[calc(12%-0.5rem)]">
+      <AddProductForm />
+    </main>
   );
 };
 export default AddProductPage;
